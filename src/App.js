7e1dc9ec62af7cdedef2fcc5c1e80214c78e93be
mvill <119754846+migavg@users.noreply.github.com>
2023-05-17 21:00:37 -0700

    added header nav bar, and bulma for css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/HelloReact';
-import HelloReact from './components/HelloReact';
+import React from 'react';
+import Header from './components/Header';
+import 'bulma/css/bulma.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <HelloReact/>
-      </header>
-      
-    </div>
-
-  );
-}
-
+const App = () => {
+      return (
+        <div classname="main"> 
+          <div className="buttons">
+           <Header/>
+          </div>
+        </div>
+      )
+  }
+  
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+const Header = () => {
+  return (
+    <div classname="main"> 
+      <div className="buttons">
+        <button class="button is-primary">About Me</button>
+        <button class="button is-primary">Portfolio</button>
+        <button class="button is-primary">Contact Me</button>
+        <button class="button is-primary">Resume</button>
+
+        
+       
+      </div>
+    </div>
+  )
+}
+
+export default Header;
+
